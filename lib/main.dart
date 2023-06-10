c52c4c9d858cf70d1f83ad05d35c3809dd4e9cad
shinnkura <n.shinzo1010@icloud.com>
2023-06-10 17:16:51 +0900

    my_home_page.dartを制作

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home/my_home_page.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -17,93 +19,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: const MyHomePage(title: 'Coffee Order Tracker', key: Key('home')),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required Key key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final _formKey = GlobalKey<FormState>();
-  final _nameController = TextEditingController();
-  String dropdownValue = 'コーヒー';
-  String timeDropdownValue = '15時';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Form(
-        key: _formKey,
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              TextFormField(
-                controller: _nameController,
-                decoration: const InputDecoration(
-                  labelText: 'Enter your name',
-                ),
-                validator: (value) {
-                  if (value?.length == 0) {
-                    return 'Please enter your name';
-                  }
-                  return null;
-                },
-              ),
-              DropdownButton<String>(
-                value: dropdownValue,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    dropdownValue = newValue!;
-                  });
-                },
-                items: <String>['コーヒー', 'ふわふわカフェオレ', 'カフェオレ']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              DropdownButton<String>(
-                value: timeDropdownValue,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    timeDropdownValue = newValue!;
-                  });
-                },
-                items: <String>['15時', '17時']
-                    .map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    print(
-                        'Name: ${_nameController.text}, Order: $dropdownValue, Time: $timeDropdownValue');
-                  }
-                },
-                child: const Text('Submit'),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
