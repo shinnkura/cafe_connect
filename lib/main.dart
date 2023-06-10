ae4a35b96a0e09165787c24a1602a51acdf58e56
shinnkura <n.shinzo1010@icloud.com>
2023-06-10 17:13:37 +0900

    debug表示を非表示。また、constを追加

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,18 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Coffee Order Tracker',
       theme: ThemeData(
         primarySwatch: Colors.brown,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Coffee Order Tracker', key: Key('home')),
+      home: const MyHomePage(title: 'Coffee Order Tracker', key: Key('home')),
     );
   }
 }
