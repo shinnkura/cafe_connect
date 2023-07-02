32287591dc6ae2bb18b0c80f2bb945cfa2a84a26
shinnkura <n.shinzo1010@icloud.com>
2023-07-02 12:19:18 +0900

    UIの改善

--- lib/screens/order_lists/order_list.dart
+++ lib/screens/order_lists/order_list.dart
@@ -34,102 +34,104 @@ class _OrderListPageState extends State<OrderListPage> {
                   int totalOrdersAtThisTime = orders[time]!
                       .values
                       .fold(0, (prev, curr) => prev + curr.length);
-                  return LayoutBuilder(
-                    builder: (context, constraints) => ExpansionTile(
-                      title: Text('$time     $totalOrdersAtThisTime名',
-                          style: TextStyle(
-                              color: Colors.brown[800], fontSize: 20)),
-                      children: orders[time]!.entries.map((entry) {
-                        String coffeeType = entry.key;
-                        List<Map<String, dynamic>> ordersList = entry.value;
-                        return Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0),
-                          ),
-                          elevation: 10,
-                          margin: const EdgeInsets.all(8.0),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  '$coffeeType     ${ordersList.length}名',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18,
-                                      color: Colors.brown[800]),
-                                ),
-                                Divider(color: Colors.brown[800]),
-                                ...ordersList.map((order) {
-                                  return Row(
-                                    mainAxisAlignment: MainAxisAlignment.start,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      IconButton(
-                                        icon: Icon(
-                                          Icons.edit,
-                                          color: Colors.brown[700],
-                                        ),
-                                        onPressed: () {
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                              builder: (context) =>
-                                                  EditOrderPage(
-                                                name: order['name'],
-                                                initialCoffeeType: coffeeType,
-                                                initialTime: time,
-                                              ),
-                                            ),
-                                          );
-                                        },
+                  return ExpansionTile(
+                    title: Text('$time     $totalOrdersAtThisTime名',
+                        style:
+                            TextStyle(color: Colors.brown[800], fontSize: 20)),
+                    children: orders[time]!.entries.map((entry) {
+                      String coffeeType = entry.key;
+                      List<Map<String, dynamic>> ordersList = entry.value;
+                      return Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        elevation: 10,
+                        margin: const EdgeInsets.all(8.0),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '$coffeeType     ${ordersList.length}名',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.brown[800]),
+                              ),
+                              Divider(color: Colors.brown[800]),
+                              ...ordersList.map((order) {
+                                return Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.edit,
+                                        color: Colors.brown[700],
                                       ),
-                                      Text.rich(
-                                        TextSpan(
-                                          children: [
-                                            TextSpan(
-                                              text: '${order['name']}',
-                                              style: TextStyle(
-                                                fontSize: 16,
-                                                color: Colors.brown[700],
-                                              ),
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => EditOrderPage(
+                                              name: order['name'],
+                                              initialCoffeeType: coffeeType,
+                                              initialTime: time,
                                             ),
-                                            TextSpan(
-                                              text:
-                                                  '${order['isSugar'] ? '   砂糖' : ''}',
-                                              style: TextStyle(
-                                                fontSize: 16,
-                                                color: Colors.red,
-                                              ),
+                                          ),
+                                        );
+                                      },
+                                    ),
+                                    Text.rich(
+                                      TextSpan(
+                                        children: [
+                                          TextSpan(
+                                            text: '${order['name']}',
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.brown[700],
                                             ),
-                                            TextSpan(
-                                              text:
-                                                  '${order['isPickupOn4thFloor'] ? '   4階で受け取る' : ''}',
-                                              style: TextStyle(
-                                                fontSize: 16,
-                                                color: Colors.blue,
-                                              ),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                '${order['isSugar'] ? '   砂糖' : ''}',
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.red,
                                             ),
-                                          ],
-                                        ),
+                                          ),
+                                          TextSpan(
+                                            text:
+                                                '${order['isPickupOn4thFloor'] ? '   4階で受け取る' : ''}',
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              color: Colors.blue,
+                                            ),
+                                          ),
+                                        ],
                                       ),
-                                    ],
-                                  );
-                                }).toList(),
-                              ],
-                            ),
+                                    ),
+                                  ],
+                                );
+                              }).toList(),
+                            ],
                           ),
-                        );
-                      }).toList(),
-                    ),
+                        ),
+                      );
+                    }).toList(),
                   );
                 },
               );
             }
           }),
+      // floatingActionButton: CustomElevatedButton(
+      //   onPressed: () {
+      //     Navigator.popUntil(context, (route) => route.isFirst);
+      //   },
+      //   text: 'ホームに戻る',
+      // ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -139,8 +141,8 @@ class _OrderListPageState extends State<OrderListPage> {
               onPressed: () async {
                 const url =
                     'http://docs.google.com/forms/d/e/1FAIpQLSc1fO0xXfhBt_h-m62Evx0wL_J_z60Xe4rfH-zvxDGnaw-9aQ/viewform';
-                if (await canLaunch(url)) {
-                  await launch(url);
+                if (await canLaunchUrl(Uri.parse(url))) {
+                  await launchUrl(Uri.parse(url));
                 } else {
                   throw 'Could not launch $url';
                 }
