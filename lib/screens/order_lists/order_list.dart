a38a80514cb27ededba58cbb06930c63db3f7db6
shinnkura <n.shinzo1010@icloud.com>
2023-07-16 13:33:47 +0900

    横スワイプでの削除機能を実装

--- lib/screens/order_lists/order_list.dart
+++ lib/screens/order_lists/order_list.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../edito_order_pages/edit_order_page.dart';
@@ -25,8 +26,11 @@ class _OrderListPageState extends State<OrderListPage> {
           } else if (snapshot.hasError) {
             return Center(child: Text('エラーが発生しました: ${snapshot.error}'));
           } else {
+            // Create a separate variable for the orders map.
             Map<String, Map<String, List<Map<String, dynamic>>>> orders =
                 snapshot.data!;
+            Map<String, Map<String, List<Map<String, dynamic>>>> ordersMap =
+                orders;
             return ListView.builder(
               itemCount: orders.length,
               itemBuilder: (context, index) {
@@ -61,80 +65,123 @@ class _OrderListPageState extends State<OrderListPage> {
                             ),
                             Divider(color: Colors.brown[800]),
                             ...ordersList.map((order) {
-                              return Row(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  IconButton(
-                                    icon: Icon(
-                                      Icons.edit,
-                                      color: Colors.brown[700],
+                              return Dismissible(
+                                key: Key(order[
+                                    'name']), // Unique key for Dismissible
+                                onDismissed: (direction) async {
+                                  // Remove the item from the Firestore.
+                                  CollectionReference orders = FirebaseFirestore
+                                      .instance
+                                      .collection('orders');
+                                  QuerySnapshot querySnapshot =
+                                      await orders.get();
+                                  for (var doc in querySnapshot.docs) {
+                                    Map<String, dynamic> data =
+                                        doc.data() as Map<String, dynamic>;
+                                    if (data['name'] == order['name'] &&
+                                        data['coffeeType'] == coffeeType &&
+                                        data['time'] == time) {
+                                      doc.reference.delete();
+                                      break;
+                                    }
+                                  }
+
+                                  // Update the state of the application to reflect the deletion.
+                                  setState(() {
+                                    ordersMap[time]![coffeeType]!.remove(order);
+                                    if (ordersMap[time]![coffeeType]!.isEmpty) {
+                                      ordersMap[time]!.remove(coffeeType);
+                                      if (ordersMap[time]!.isEmpty) {
+                                        ordersMap.remove(time);
+                                      }
+                                    }
+                                  });
+
+                                  // Then show a snackbar.
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                        content:
+                                            Text("${order['name']} dismissed")),
+                                  );
+                                },
+                                background: Container(
+                                    color:
+                                        Colors.red), // Red background on swipe
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    IconButton(
+                                      icon: Icon(
+                                        Icons.edit,
+                                        color: Colors.brown[700],
+                                      ),
+                                      onPressed: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => EditOrderPage(
+                                              name: order['name'],
+                                              initialCoffeeType: coffeeType,
+                                              initialTime: time,
+                                            ),
+                                          ),
+                                        );
+                                      },
                                     ),
-                                    onPressed: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => EditOrderPage(
-                                            name: order['name'],
-                                            initialCoffeeType: coffeeType,
-                                            initialTime: time,
+                                    Row(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          '${order['name']}',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.brown[700],
                                           ),
                                         ),
-                                      );
-                                    },
-                                  ),
-                                  Row(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        '${order['name']}',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.brown[700],
+                                        Text(
+                                          order['small'] ? '  少なめ' : '',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.orange,
+                                          ),
                                         ),
-                                      ),
-                                      Text(
-                                        order['small'] ? '  少なめ' : '',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.orange,
+                                        Text(
+                                          order['isSugar'] ? '  砂糖' : '',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.red,
+                                          ),
                                         ),
-                                      ),
-                                      Text(
-                                        order['isSugar'] ? '  砂糖' : '',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.red,
+                                        Text(
+                                          order['caramel'] ? '  キャラメル' : '',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.purple,
+                                          ),
                                         ),
-                                      ),
-                                      Text(
-                                        order['caramel'] ? '  キャラメル' : '',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.purple,
+                                        Text(
+                                          order['isCondecensedMilk']
+                                              ? '  練乳'
+                                              : '',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.blue,
+                                          ),
                                         ),
-                                      ),
-                                      Text(
-                                        order['isCondecensedMilk']
-                                            ? '  練乳'
-                                            : '',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.blue,
+                                        Text(
+                                          order['isPickupOn4thFloor']
+                                              ? '  4階受取'
+                                              : '',
+                                          style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.green,
+                                          ),
                                         ),
-                                      ),
-                                      Text(
-                                        order['isPickupOn4thFloor']
-                                            ? '  4階受取'
-                                            : '',
-                                        style: TextStyle(
-                                          fontSize: 16,
-                                          color: Colors.green,
-                                        ),
-                                      ),
-                                    ],
-                                  )
-                                ],
+                                      ],
+                                    )
+                                  ],
+                                ),
                               );
                             }).toList(),
                           ],
