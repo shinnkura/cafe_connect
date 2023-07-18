f69a9e4a25e675bb116ba2575b1aa21aa6b82a72
shinnkura <n.shinzo1010@icloud.com>
2023-07-18 22:33:14 +0900

    注文画面で注文ボタンせない問題の解消

--- lib/screens/order_screen/order_screen.dart
+++ lib/screens/order_screen/order_screen.dart
@@ -63,140 +63,143 @@ class _OrderPageState extends State<OrderPage> {
   }
 
   Widget _buildBody() {
-    return Form(
-      key: _formKey,
-      child: Padding(
-        padding: const EdgeInsets.all(20.0 * 1.5),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: TextFormField(
-                controller: _nameController,
-                style: Theme.of(context).textTheme.titleMedium,
-                decoration: const InputDecoration(
-                  labelText: '名前',
-                  labelStyle: TextStyle(color: kTextColor),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: kTextColor),
+    return SingleChildScrollView(
+      child: Form(
+        key: _formKey,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0 * 1.5),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: TextFormField(
+                  controller: _nameController,
+                  style: Theme.of(context).textTheme.titleMedium,
+                  decoration: const InputDecoration(
+                    labelText: '名前',
+                    labelStyle: TextStyle(color: kTextColor),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: kTextColor),
+                    ),
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'お名前をご記入ください';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    _formKey.currentState!.validate();
+                  },
                 ),
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'お名前をご記入ください';
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: CustomDropdownButton<String>(
+                  value: timeDropdownValue,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      timeDropdownValue = newValue!;
+                    });
+                  },
+                  items: const ['15時30分', '17時30分'],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: CustomDropdownButton<String>(
+                  value: dropdownValue,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue = newValue!;
+                    });
+                  },
+                  items: const [
+                    'コーヒー',
+                    'カフェオレ',
+                    'ちょいふわカフェオレ',
+                    'ふわふわカフェオレ',
+                    'アイスコーヒー（水出し）',
+                    'アイスコーヒー(急冷式)',
+                    'アイスカフェオレ',
+                    'アイスカフェオレ（ミルク多め）',
+                    'ソイラテ',
+                    'アイスソイラテ',
+                    '温かい緑茶',
+                    '冷たい緑茶',
+                  ],
+                ),
+              ),
+              CheckboxListTile(
+                title: const Text("少なめ（250ml程度）"),
+                value: small,
+                onChanged: (bool? value) {
+                  setState(() {
+                    small = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text("砂糖あり"),
+                value: _isSugar,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _isSugar = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text("キャラメルシロップ"),
+                value: caramel,
+                onChanged: (bool? value) {
+                  setState(() {
+                    caramel = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text("練乳あり"),
+                value: _isCondecensedMilk,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _isCondecensedMilk = value!;
+                  });
+                },
+              ),
+              CheckboxListTile(
+                title: const Text("４階で受け取る"),
+                value: _isPickupOn4thFloor,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _isPickupOn4thFloor = value!;
+                  });
+                },
+              ),
+              CustomElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _saveOrder(
+                      timeDropdownValue,
+                      dropdownValue,
+                      _nameController.text,
+                      small,
+                      _isSugar,
+                      caramel,
+                      _isCondecensedMilk,
+                      _isPickupOn4thFloor,
+                    );
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ThanksPage()),
+                    );
                   }
-                  return null;
                 },
-                onChanged: (value) {
-                  _formKey.currentState!.validate();
-                },
+                text: '注文',
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: CustomDropdownButton<String>(
-                value: timeDropdownValue,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    timeDropdownValue = newValue!;
-                  });
-                },
-                items: const ['15時30分', '17時30分'],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: CustomDropdownButton<String>(
-                value: dropdownValue,
-                onChanged: (String? newValue) {
-                  setState(() {
-                    dropdownValue = newValue!;
-                  });
-                },
-                items: const [
-                  'コーヒー',
-                  'カフェオレ',
-                  'ちょいふわカフェオレ',
-                  'ふわふわカフェオレ',
-                  'アイスコーヒー（水出し）',
-                  'アイスコーヒー(急冷式)',
-                  'アイスカフェオレ',
-                  'アイスカフェオレ（ミルク多め）',
-                  'ソイラテ',
-                  'アイスソイラテ',
-                  '温かい緑茶',
-                  '冷たい緑茶',
-                ],
-              ),
-            ),
-            CheckboxListTile(
-              title: const Text("少なめ（250ml程度）"),
-              value: small,
-              onChanged: (bool? value) {
-                setState(() {
-                  small = value!;
-                });
-              },
-            ),
-            CheckboxListTile(
-              title: const Text("砂糖あり"),
-              value: _isSugar,
-              onChanged: (bool? value) {
-                setState(() {
-                  _isSugar = value!;
-                });
-              },
-            ),
-            CheckboxListTile(
-              title: const Text("キャラメルシロップ"),
-              value: caramel,
-              onChanged: (bool? value) {
-                setState(() {
-                  caramel = value!;
-                });
-              },
-            ),
-            CheckboxListTile(
-              title: const Text("練乳あり"),
-              value: _isCondecensedMilk,
-              onChanged: (bool? value) {
-                setState(() {
-                  _isCondecensedMilk = value!;
-                });
-              },
-            ),
-            CheckboxListTile(
-              title: const Text("４階で受け取る"),
-              value: _isPickupOn4thFloor,
-              onChanged: (bool? value) {
-                setState(() {
-                  _isPickupOn4thFloor = value!;
-                });
-              },
-            ),
-            CustomElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  _saveOrder(
-                    timeDropdownValue,
-                    dropdownValue,
-                    _nameController.text,
-                    small,
-                    _isSugar,
-                    caramel,
-                    _isCondecensedMilk,
-                    _isPickupOn4thFloor,
-                  );
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const ThanksPage()),
-                  );
-                }
-              },
-              text: '注文',
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
