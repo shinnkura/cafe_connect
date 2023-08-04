3128a0ead892a2948d130648bcc5e3fa09928ce2
shinnkura <n.shinzo1010@icloud.com>
2023-08-04 22:40:12 +0900

    update：ホームスクリーンのUIを改善

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -9,26 +9,55 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        title: const Text('Coffee Order'),
-        backgroundColor: Colors.brown,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const GreetingText(),
-            const SizedBox(height: 20),
-            const OrderButton(),
-            const SizedBox(height: 200),
-            CustomElevatedButton(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        shadowColor: Colors.transparent,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: IconButton(
+              icon: Icon(Icons.coffee, color: Colors.white),
               onPressed: () {
                 Navigator.pushNamed(context, '/orderList');
               },
-              text: '注文一覧',
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 1.0,
+            height: MediaQuery.of(context).size.height * 1.0,
+            child: ColorFiltered(
+              colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.6), BlendMode.darken),
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      'https://images.unsplash.com/photo-1510972527921-ce03766a1cf1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const GreetingText(),
+                const SizedBox(height: 20),
+                const OrderButton(),
+                const SizedBox(height: 200),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
