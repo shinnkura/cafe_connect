9def75edfd575047cd12a1c8b5de085d04bbb779
shinnkura <n.shinzo1010@icloud.com>
2023-08-26 21:06:03 +0900

    Update：初期画面でLottieを追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:cafe_connect/screens/order_screen.dart';
 import 'package:cafe_connect/screens/order_list.dart';
 import 'package:device_preview/device_preview.dart';
@@ -5,6 +6,8 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:lottie/lottie.dart';
+import 'package:page_transition/page_transition.dart';
 import 'firebase_options.dart';
 
 import 'constants.dart';
@@ -24,6 +27,28 @@ void main() async {
   );
 }
 
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Lottie.network(
+        'https://lottie.host/c377cb11-db7c-441f-b444-683224969d58/TfSIr1spOc.json',
+        height: 250,
+        width: 250,
+      ),
+      backgroundColor: Colors.brown,
+      nextScreen: HomePage(),
+      splashIconSize: 250,
+      duration: 3000,
+      splashTransition: SplashTransition.fadeTransition,
+      pageTransitionType: PageTransitionType.leftToRightWithFade,
+      animationDuration: const Duration(milliseconds: 1500),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -33,7 +58,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Coffee Order',
       theme: _buildThemeData(context),
-      home: HomePage(),
+      home: SplashScreen(),
       routes: _buildRoutes(),
     );
   }
